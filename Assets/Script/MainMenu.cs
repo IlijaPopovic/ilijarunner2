545bb787228da66d6b877bae499c09a85e240561
ilija <popovic.ilija.99@gmail.com>
2020-02-26 00:49:26 +0100

    dodat main menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    List<GameObject> PanelOrder = new List<GameObject>();
+
+    [SerializeField] private GameObject Options;
+    [SerializeField] private GameObject Leaderboard;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
