4e86705974dda516959be29c130823e49a8656e9
IlijaPopovic <ilija.popovic@cofamedia.com>
2020-02-26 16:23:01 +0100

    promena klika na UI

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,16 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
-    List<GameObject> PanelOrder = new List<GameObject>();
-
+    [SerializeField] private Button OptionsButton;
+    [SerializeField] private Button LeaderboardButton;
+    [SerializeField] private Button StartButton;
+    [SerializeField] private Button OptionsBackButton;
+    [SerializeField] private Button LeaderboardBackButton;
     [SerializeField] private GameObject Options;
     [SerializeField] private GameObject Leaderboard;
 
-    public void StartGame()
+
+    void Start()
+    {
+        OptionsButton.onClick.AddListener(OnOptionsButtonClick);
+        LeaderboardButton.onClick.AddListener(OnLeaderboardButtonClick);
+        StartButton.onClick.AddListener(OnStartButtonClick);
+        OptionsBackButton.onClick.AddListener(OnOptionsBackButtonClick);
+        LeaderboardBackButton.onClick.AddListener(OnLeaderboardBackButtonClick);
+    }
+
+    public void OnStartButtonClick()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    private void OnOptionsButtonClick()
+    {
+        Options.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+    private void OnOptionsBackButtonClick()
+    {
+        Options.SetActive(false);
+        this.gameObject.SetActive(true);
+    }
+
+    private void OnLeaderboardButtonClick()
+    {
+        Leaderboard.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnLeaderboardBackButtonClick()
+    {
+        Leaderboard.SetActive(false);
+        this.gameObject.SetActive(true);
     }
 }
